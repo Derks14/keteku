95f0df6ce91a657e14e9e0d9ac5dbfb708e654d5
4teen <meet4teen@gmail.com>
2024-12-24 16:53:03 +1100

    added dummy projects. fixed space around footer content

--- src/app/(front)/projects/page.tsx
+++ src/app/(front)/projects/page.tsx
@@ -1,5 +1,55 @@
+import Wrapper from "@/components/ui/wrapper";
+import Card from "@/components/ui/card";
+
 export default function Lab() {
+  const projects = [
+    {
+      id: 324,
+      span: 2,
+      project: "Lab",
+      title: "",
+    },
+    {
+      id: 14,
+      span: 2,
+      project: "Lab",
+      title: "",
+    },
+    {
+      id: 321,
+      span: 2,
+      project: "Lab",
+      title: "",
+    },
+    {
+      id: 322,
+      span: 2,
+      project: "Lab",
+      title: "",
+    },
+    {
+      id: 394,
+      span: 2,
+      project: "Lab",
+      title: "",
+    },
+    {
+      id: 354,
+      span: 2,
+      project: "Lab",
+      title: "",
+    },
+  ];
   return (
-    <div> This page contains experiments thats been happening in the lab </div>
-  )
+    <>
+      <Wrapper page="Projects" row_cols_class="md:grid-cols-4 md:grid-rows-3">
+        {projects.map((project) => (
+          <Card className="" key={project.id}>
+            <div>aws</div>
+            <div>DEPLOYING KUBENETES</div>
+          </Card>
+        ))}
+      </Wrapper>
+    </>
+  );
 }
