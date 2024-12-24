d3037135a6d0b38b4f3325eb2da66e9d8b4b5290
4teen <meet4teen@gmail.com>
2024-12-25 01:09:35 +1100

    fix footer and top navigation spacing

--- src/components/sections/top-nav.tsx
+++ src/components/sections/top-nav.tsx
@@ -4,15 +4,15 @@ interface TopNavProps {
 
 const topNav = ({ title }: TopNavProps) => {
   return (
-    <div>
-      <div className="flex  justify-between">
+    <>
+      <div className="flex justify-between pb-6">
         <div>
           <span>do it with shadow</span>
           <h1 className="text-6xl ">{title}</h1>
         </div>
         <div>toggle day and night</div>
       </div>
-    </div>
+    </>
   );
 };
 export default topNav;
